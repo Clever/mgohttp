cc8af3a522fb2774865e57807f7b201e4d7b8045
Nate Brennand <nate.brennand@clever.com>
2018-03-09 13:09:39 -0800

    lazy session generation

--- mgosessionpool/sessionpool.go
+++ mgosessionpool/sessionpool.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"github.com/Clever/app-service/config"
-	"golang.org/x/sync/semaphore"
 	"gopkg.in/Clever/kayvee-go.v6/logger"
 	mgo "gopkg.in/mgo.v2"
 )
@@ -32,16 +31,19 @@ type MongoSessionInjectorConfig struct {
 	Handler      http.Handler
 }
 
+type mgoSessionCopier interface {
+	Copy() *mgo.Session
+}
+
 // MongoSessionInjector is an HTTP middleware that injects a new copied mongo session
 // into the Context of the request.
 // This middleware handles timing out inflight Mongo requests.
 type MongoSessionInjector struct {
-	parentSession *mgo.Session
+	parentSession mgoSessionCopier
 	sessionLimit  int64
 	database      string
 	timeout       time.Duration
 	handler       http.Handler
-	sem           *semaphore.Weighted
 }
 
 // NewMongoSessionInjector TODO
@@ -51,47 +53,56 @@ func NewMongoSessionInjector(cfg MongoSessionInjectorConfig) http.Handler {
 		parentSession: cfg.Sess,
 		timeout:       cfg.Timeout,
 		handler:       cfg.Handler,
-		sem:           semaphore.NewWeighted(cfg.SessionLimit),
 	}
 }
 
+type sessionGetter func() *mgo.Session
+
 func (c *MongoSessionInjector) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
+	// Instantiate the nil session and timer objects that may be lazily instantiated if
+	// the request handler asks for a session.
+	var newSession *mgo.Session
+	var sessionTimer *time.Timer
 
-	if err := c.sem.Acquire(ctx, 1); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(fmt.Sprintf("mongo pool exhausted at %d conns", c.sessionLimit)))
-		return
+	// getSession is injected into the Context, repeatedly calls by the same request will return
+	// the same session.
+	var getSession sessionGetter = func() *mgo.Session {
+		// we've already created a session for this request, shortcircuit and return that session.
+		if newSession != nil {
+			return newSession
+		}
+
+		// create a session copy
+		newSession = c.parentSession.Copy()
+
+		// SetSocketTimeout guarantees that no individual query to mongo can take longer than
+		// the RequestTimeoutDuration value.
+		newSession.SetSocketTimeout(config.RequestTimeoutDuration)
+
+		// We close the mongo session after RequestTimeoutDuration, this means that no new queries
+		// can be opened
+		sessionTimer = time.AfterFunc(config.RequestTimeoutDuration, func() {
+			logger.FromContext(r.Context()).Error("mongo-session-killed")
+			newSession.Close()
+		})
+
+		return newSession
 	}
-	defer c.sem.Release(1)
-
-	// create a session copy
-	newSession := c.parentSession.Copy()
-
-	// SetSocketTimeout guarantees that no individual query to mongo can take longer than
-	// the RequestTimeoutDuration value.
-	newSession.SetSocketTimeout(config.RequestTimeoutDuration)
-
-	// We close the mongo session after RequestTimeoutDuration, this means that no new queries
-	// can be opened
-	timer := time.AfterFunc(config.RequestTimeoutDuration, func() {
-		logger.FromContext(r.Context()).Error("mongo-session-killed")
-		newSession.Close()
-	})
 
 	// amend the request context with the database connection then serve the wrapped
 	// HTTP handler
-	r = r.WithContext(context.WithValue(r.Context(), getMgoSessionKey(c.database), newSession))
-	c.handler.ServeHTTP(w, r)
+	amendedReq := r.WithContext(context.WithValue(r.Context(), getMgoSessionKey(c.database), getSession))
+	c.handler.ServeHTTP(w, amendedReq)
 
-	// Try stopping the timer. If we succeed, close the session
-	if timer.Stop() {
+	// Try stopping the timer if there is one. If we succeed, close the session
+	if sessionTimer != nil && sessionTimer.Stop() {
 		logger.FromContext(r.Context()).Info("regular-mongo-session-close")
 		newSession.Close()
 	}
 }
 
-// LimitedSession is the methods from an *mgo.Session that we give the consumer access to
+// LimitedSession is the methods from an *mgo.Session that we give the consumer access to. We
+// omit Close because that is the responsibility of the MongoSessionInjector
 type LimitedSession interface {
 	DB(string) *mgo.Database
 	Ping() error
@@ -99,9 +110,10 @@ type LimitedSession interface {
 
 // SessionFromContext retrieves a *mgo.Session from the request context.
 func SessionFromContext(ctx context.Context, database string) LimitedSession {
-	sessionBlob := ctx.Value(getMgoSessionKey(database))
-	if sess, ok := sessionBlob.(*mgo.Session); ok {
-		return sess
+	getSessionBlob := ctx.Value(getMgoSessionKey(database))
+	if getSession, ok := getSessionBlob.(sessionGetter); ok {
+		return getSession()
 	}
+
 	panic(fmt.Sprintf("SessionFromContext must receive a valid database name: %s not found", database))
 }
